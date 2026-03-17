0913e2482a7b00200f8e397dfa68a8ca2e57bc99
1 <a1@MacBook-Pro-1.local>
2026-03-17 07:56:14 +0300

    Initial commit

---
+++ src/main/java/by/javaguru/jdmik12/streamingservice/StreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package by.javaguru.jdmik12.streamingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/javaguru/jdmik12/streamingservice/StreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.javaguru.jdmik12.streamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
